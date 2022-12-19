7fb526db6273a1bed45d697bb42662c1442007eb
Pencari Ilmu <83212789+otherlife-code@users.noreply.github.com>
2022-12-19 07:11:52 +0700

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,28 @@
+from confluent_kafka.avro import AvroConsumer
+
+# Define function to read each record of dataset sent by kafka producer
+def read_messages():
+    consumer_config = {"bootstrap.servers": "localhost:9092",
+                       "schema.registry.url": "http://localhost:8081",
+                       "group.id": "com.iykra.datafellowship",
+                       "auto.offset.reset": "earliest"} # untuk sequence
+
+    consumer = AvroConsumer(consumer_config)
+    consumer.subscribe(['kafka_avroproducer'])
+
+    while(True):
+        try:
+            message = consumer.poll(5)
+        except Exception as e:
+            print(f"Exception while trying to poll messages - {e}")
+        else:
+            if message:
+                print(f"Successfully poll a record from "
+                      f"Kafka topic: {message.topic()}, partition: {message.partition()}, offset: {message.offset()}\n"
+                      f"message key: {message.key()} || message value: {message.value()}")
+                consumer.commit()
+            else:
+                print("No new messages at this point. Try again later.")
+        # consumer.close()
+
+if __name__ == "__main__":
